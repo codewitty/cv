08f4650b456c3277f9ebdf0771cfa8aebcbc977f
Joshua Gomes <gomesjoshua@gmail.com>
2020-08-29 23:06:06 -0400

    new with nodemon

--- my-express-server/server.js
+++ my-express-server/server.js
@@ -2,10 +2,18 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", function(request, response){
-    response.send("Hello");
-
+app.get("/", function(req, res){
+    res.send("Hello");
 });
+
+app.get("/about", function(req, res){
+    res.send("My name is Auhsoj Semog");
+});
+
+app.get("/resume", function(req, res){
+    res.send("Find my resume below:");
+});
+
 app.listen(3000, function(){
     console.log("Server started on port 3000");
 });
