72d56d6a846a6e46dc97063f09900c8eb6afb74b
Joshua Gomes <gomesjoshua@gmail.com>
2020-09-11 10:57:37 -0700

    weather app done.

--- WeatherProject/app.js
+++ WeatherProject/app.js
@@ -1,24 +1,20 @@
 const express = require("express");
 const https = require("https");
+const bodyParser = require("body-parser");
 
 const app = express();
 
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.get("/", function(req, res){
     res.sendFile(__dirname + "/index.html");
-
-
-})
+});
 
 app.post("/", function(req, res){
-    console.log("Posted");
-})
-
-
-    /*
-    const query = "Santa Clara"
-    const units = "metric"
-    const apiKey = "9e45703d4ebce98a50d6970fda54c452"
-    const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&units=" + units + "&appid=" + apiKey 
+    const query = req.body.cityName;
+    const units = "metric";
+    const apiKey = "9e45703d4ebce98a50d6970fda54c452";
+    const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&units=" + units + "&appid=" + apiKey;
     https.get(url, function(response){
         console.log(response.statusCode);
 
@@ -36,7 +32,9 @@ app.post("/", function(req, res){
         res.send();
         })
     })
-    */
+})
+
+
 
 app.listen(3000, function(){
     console.log("Server started on port 3000");
