4a9e17afae66f8955d9ebebcafbc8fe867ae7948
Joshua Gomes <gomesjoshua@gmail.com>
2020-09-12 21:57:46 -0700

    changes

---
+++ Newsletter-Signup/app.js
@@ -0,0 +1,77 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+const https = require("https");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+app.get("/", function(req, res){
+    res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", function(req, res){
+    const firstName = req.body.fName;
+    const lastName = req.body.lName;
+    const email = req.body.email;
+    console.log(firstName, lastName, email);
+
+    const data = {
+        members: [
+        {
+            email_address: email,
+            status: "subscribed",
+            merge_fields: {
+                FNAME: firstName,
+                LNAME: lastName
+            }
+        }
+        ]
+    }
+
+    const jsonData = JSON.stringify(data);
+    const url = "https://us2.api.mailchimp.com/3.0/lists/9e4908dfc6?" 
+    const options = { method: "POST", auth: "joshua:6c2627bc9853af41e1946978e0ae26a7-us2" } 
+    
+    const request = https.request(url, options, function(response){
+        response.on("data"), function(data){
+            console.log(JSON.parse(data));
+        }
+
+        request.write(statusCode);
+
+        request.write(jsonData);
+
+        req.end();
+
+    })
+});
+
+/*
+const client = require("mailchimp-marketing");
+
+client.setConfig({
+  apiKey: "YOUR_API_KEY",
+  server: "YOUR_SERVER_PREFIX",
+});
+
+const run = async () => {
+  const response = await client.lists.batchListMembers("list_id", {
+    members: [{}],
+  });
+  console.log(response);
+};
+
+run();
+*/
+
+app.listen(3000, function(){
+    console.log("Server started on port 3000");
+});
+
+
+//6c2627bc9853af41e1946978e0ae26a7-us2
+//list id
+//
